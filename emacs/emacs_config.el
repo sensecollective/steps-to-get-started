731e2cc28c328db05ebf0a7a99e40269fe955e28
Archit Sharma <arcsharm@redhat.com>
2014-10-13 16:04:42 +0530

    update emacs config and docker tut

--- emacs/emacs_config.el
+++ emacs/emacs_config.el
@@ -72,3 +72,32 @@ smtpmail-debug-info t)
   ;; For important compatibility libraries like cl-lib
   (add-to-list 'package-archives '("gnu" . "http://elpa.gnu.org/packages/")))
 (package-initialize)
+
+
+;; ================= web Mode ====================
+
+(load-file "~/.emacs.d/web-mode.el")
+(require 'web-mode) 
+(add-to-list 'auto-mode-alist '("\\.phtml\\'" . web-mode)) 
+(add-to-list 'auto-mode-alist '("\\.tpl\\.php\\'" . web-mode)) 
+(add-to-list 'auto-mode-alist '("\\.[gj]sp\\'" . web-mode)) 
+(add-to-list 'auto-mode-alist '("\\.as[cp]x\\'" . web-mode)) 
+(add-to-list 'auto-mode-alist '("\\.erb\\'" . web-mode)) 
+(add-to-list 'auto-mode-alist '("\\.mustache\\'" . web-mode)) 
+(add-to-list 'auto-mode-alist '("\\.djhtml\\'" . web-mode))
+(add-to-list 'auto-mode-alist '("\\.html?\\'" . web-mode))
+
+(defun my-web-mode-hook () 
+  "Hooks for Web mode." 
+  (setq web-mode-markup-indent-offset 2) 
+  ) 
+(add-hook 'web-mode-hook 'my-web-mode-hook)
+
+(setq web-mode-style-padding 1)
+(setq web-mode-script-padding 1)
+(setq web-mode-block-padding 0)
+(setq web-mode-comment-style 2)
+
+(setq web-mode-markup-indent-offset 2)
+(setq web-mode-css-indent-offset 2)
+(setq web-mode-code-indent-offset 2)
