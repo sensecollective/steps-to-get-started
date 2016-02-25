8586dc4edac4303db39f30d1b802317bc1ef2813
Archit Sharma <arcsharm@redhat.com>
2016-02-25 07:39:20 +0530

    add callback and grep example in nodejs

---
+++ tutorials/nodejs_grep_callback.js
@@ -0,0 +1,22 @@
+
+// install: $ npm install simple-grep
+
+var grep = require('simple-grep');
+var test_var = "archit";
+var test_var_2 = "fullmetal";
+var xyz_endpoint = '';
+
+function callback(ff){
+  grep('UUID', '/etc/fstab', function(list, callback){
+    var _tmp = list[0].results[0];
+    var line = _tmp.line_number + ":" + _tmp.line;
+    xyz_endpoint = line.split('UUID=')[1];
+    console.log('inside: ' + xyz_endpoint + " -- " + test_var + " -- " + test_var_2);
+    ff();
+  });
+};
+
+callback(function(){
+  console.log('outside: ' + xyz_endpoint + " -- " + test_var + " -- " + test_var_2);
+  console.log('super: ' + xyz_endpoint + " -- " + test_var + " -- " + test_var_2);
+});
