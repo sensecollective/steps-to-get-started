73f24e46632e166ea04a8d5b298592e2bd1876c4
Archit Sharma <arcsharm@redhat.com>
2016-03-13 13:49:07 +0530

    add gnome ui example

---
+++ tutorials/zenity_basic_ui_gnome.sh
@@ -0,0 +1,20 @@
+#!/bin/sh
+
+zenity --forms --title="Add Friend" \
+    --text="Enter information about your friend." \
+    --separator="," \
+    --add-entry="First Name" \
+    --add-entry="Family Name" \
+    --add-entry="Email" \
+    --add-calendar="Birthday" >> addr.csv
+
+case $? in
+    0)
+        echo "Friend added.";;
+    1)
+        echo "No friend added."
+	;;
+    -1)
+        echo "An unexpected error has occurred."
+	;;
+esac
